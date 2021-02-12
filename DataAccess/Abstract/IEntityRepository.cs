c5fa478031a8ce618899dd98d8172b767563859b
irmak12 <irmakcitrak@gmail.com>
2021-02-12 16:34:00 +0300

    12.2.2021

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T : class, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        List<T> GetAllByCategory(int id);
+        T Get(Expression<Func<T, bool>> filter);
+    }
+}
+ 
---
+++ DataAccess/Concrete/EntityFramework/RentCarContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class RentCarContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"");
+        }
+    }
+}
